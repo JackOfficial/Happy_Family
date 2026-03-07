bad4d21f22fa6d3ab15233d34339b8cb0a12b6b1
JackOfficial <musengimanajacques@gmail.com>
2026-03-07 17:28:22 +0200

    Adding edit partner

--- resources/views/livewire/admin/file-manager.blade.php
+++ resources/views/livewire/admin/file-manager.blade.php
@@ -123,7 +123,7 @@
         </div>
 
         <div class="d-flex justify-content-center mt-4 mb-5">
-            {{ $paginator->links() }}
+           {{ $files->links('pagination::bootstrap-4') }}
         </div>
 
         <div x-show="showModal" class="modal-backdrop-custom" @click.self="showModal = false" x-transition.opacity>
