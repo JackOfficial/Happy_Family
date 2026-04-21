e5b168eb8c2ed491f560831191dccd62e37e6ccb
JackOfficial <musengimanajacques@gmail.com>
2026-04-21 09:30:21 +0200

    fixing

--- resources/views/components/partials/navbar.blade.php
+++ resources/views/components/partials/navbar.blade.php
@@ -25,45 +25,48 @@
     </div>
 
     <div class="container px-3 px-lg-0">
-        <nav class="navbar navbar-expand-xl navbar-light bg-transparent px-0">
-            <div class="navbar-brand">
-                <livewire:organization.logo/>
-            </div>
+      <nav class="navbar navbar-expand-xl navbar-dark bg-transparent px-0 w-100">
+    <div class="navbar-brand">
+        <livewire:organization.logo/>
+    </div>
 
-            <button class="navbar-toggler border-0 shadow-none" type="button" @click="mobileMenuOpen = !mobileMenuOpen">
-                <div class="hamburger-icon" :class="mobileMenuOpen ? 'open' : ''">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </button>
+    {{-- Use layout's mobileMenuOpen logic if needed, or stick to data-bs-target for simplicity --}}
+    <button class="navbar-toggler border-0 shadow-none" type="button" 
+            data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <div class="hamburger-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </button>
 
-            <div class="collapse navbar-collapse" :class="{ 'show': mobileMenuOpen }" id="navbarCollapse">
-                <div class="navbar-nav ms-auto align-items-center">
-                    <a href="/" class="nav-link mx-2 {{ Request::is('/') ? 'active' : '' }}">Home</a>
-                    <a href="/about" class="nav-link mx-2 {{ Request::is('about*') ? 'active' : '' }}">About</a>
-                    <a href="/causes" class="nav-link mx-2 {{ Request::is('causes*') ? 'active' : '' }}">Causes</a>
-                    <a href="{{ route('projects.index') }}" class="nav-link mx-2 {{ Request::is('projects*') ? 'active' : '' }}">Projects</a>
-                    
-                    <div class="nav-item dropdown" x-data="{ open: false }" @mouseenter="open = true" @mouseleave="open = false">
-                        <a href="#" class="nav-link dropdown-toggle mx-2" :class="{ 'active': open }">Resources</a>
-                        <div class="dropdown-menu border-0 shadow-premium m-0 animated-dropdown" :class="{ 'show': open }">
-                            <a href="/volunteers" class="dropdown-item py-2 px-4">Volunteers</a>
-                            <a href="/careers" class="dropdown-item py-2 px-4">Careers</a>
-                            <a href="/gallery" class="dropdown-item py-2 px-4">Gallery</a>
-                        </div>
-                    </div>
-
-                    <a href="/contact" class="nav-link mx-2 {{ Request::is('contact*') ? 'active' : '' }}">Contact</a>
-
-                    <div class="ms-xl-4 mt-4 mt-xl-0">
-                        <a href="/donate" class="nav-donate-btn shadow-sm d-flex align-items-center">
-                            <span>Donate Now</span>
-                            <i class="fas fa-heart ms-2"></i>
-                        </a>
-                    </div>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto align-items-center">
+            <a href="/" class="nav-link mx-2 {{ Request::is('/') ? 'active' : '' }}">Home</a>
+            <a href="/about" class="nav-link mx-2 {{ Request::is('about*') ? 'active' : '' }}">About</a>
+            <a href="/causes" class="nav-link mx-2 {{ Request::is('causes*') ? 'active' : '' }}">Causes</a>
+            <a href="{{ route('projects.index') }}" class="nav-link mx-2 {{ Request::is('projects*') ? 'active' : '' }}">Projects</a>
+            
+            <div class="nav-item dropdown" x-data="{ open: false }" @mouseenter="open = true" @mouseleave="open = false">
+                <a href="#" class="nav-link dropdown-toggle mx-2" :class="{ 'active': open }">Resources</a>
+                <div class="dropdown-menu border-0 shadow-premium m-0 animated-dropdown" :class="{ 'show': open }">
+                    <a href="/volunteers" class="dropdown-item py-2 px-4">Volunteers</a>
+                    <a href="/careers" class="dropdown-item py-2 px-4">Careers</a>
+                    <a href="/gallery" class="dropdown-item py-2 px-4">Gallery</a>
                 </div>
             </div>
-        </nav>
+
+            <a href="/contact" class="nav-link mx-2 {{ Request::is('contact*') ? 'active' : '' }}">Contact</a>
+
+            <div class="ms-xl-4 mt-4 mt-xl-0">
+                <a href="/donate" class="nav-donate-btn shadow-sm d-flex align-items-center">
+                    <span>Donate Now</span>
+                    <i class="fas fa-heart ms-2"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</nav>
     </div>
 </div>
+
